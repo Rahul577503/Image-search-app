0d226c846595c73cb026cfce67816c08217f8109
Rahul Maurya <rahulmaurya109626@gmail.com>
2023-09-17 10:14:37 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Image-search-app
  
+![App Logo](assets/Screenshot%20from%202023-09-17%2010-06-34.png)
+
+The Search Image App is a web application that allows users to search for images using keywords and display the search results from the Unsplash API.
+
+##Here is Example...
+![App Logo](assets/Screenshot%20from%202023-09-17%2010-07-24.png)
+
+
+
+## Features 🚀
+
+- Search for high-quality images by entering keywords.
+- Display search results with image thumbnails and links to the source on Unsplash.
+- Load additional images with a "Load More" button for an extensive image search experience.
+
+## Technologies Used 💻
+
+- HTML
+- CSS
+- JavaScript
+- Unsplash API
+
+## Getting Started 🚗
+
+To get started with the Search Image App, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Rahul577503/search-image-search-app.git
+   ```
---
+++ app.js
@@ -0,0 +1,56 @@
+const accesKey = "JJj7e84Y43NuYuDxA2LIvxP5acI4kLGMCn140KETtXw";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+
+const showMoreBtn = document.getElementById("show-more-btn"); 
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesKey}`;
+  const response = await fetch(url);
+  const data = await response.json(); 
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => { 
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('search-result');
+    const image = document.createElement('img');
+
+    image.src = result.urls.small; 
+    image.alt = result.alt_description; 
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html; 
+    imageLink.target = '_blank';
+    imageLink.textContent = result.alt_description; 
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper); 
+  });
+
+  page++;
+
+  if (page > 1) {
+    showMoreBtn.style.display = "block";
+  }
+}
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  searchImages();
+});
